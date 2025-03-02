c6a6d91358a4d63f134567fccf6fff016b2862f9
Ruslan Sanakoev <mrsanakoev@gmail.com>
2025-03-02 13:13:47 +0000

    chore: Update Dalle3 and Gpt4 components to use new model names and improve error handling

--- src/pages/Dalle3.js
+++ src/pages/Dalle3.js
@@ -1,19 +1,18 @@
-import React, { useState } from 'react';
-import { Button, Container, Form } from 'react-bootstrap';
-import ChatContainer from '../components/ResponseBox';
-import axios from 'axios';
-import useAuth from '../hooks/useAuth';
+import React, { useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import ChatContainer from "../components/ResponseBox";
+import axios from "axios";
+import useAuth from "../hooks/useAuth";
 const { Configuration, OpenAIApi } = require("openai");
 
 function Language4() {
-
-  const [content, setMessage] = useState('');
+  const [content, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
   const [chatHistory, addChat] = useState([]);
   const [error, setError] = useState();
   const { auth } = useAuth();
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     setMessage(event.target.value);
   };
 
@@ -23,74 +22,89 @@ function Language4() {
       apiKey: process.env.REACT_APP_OPEN_API_KEY,
     });
     const openai = new OpenAIApi(configuration);
-    let message = {messages: [{role: "user", content: content}],}
+    let message = { messages: [{ role: "user", content: content }] };
 
     let arrayInput = chatHistory;
     arrayInput.push(message.messages[0]);
     addChat(arrayInput);
 
-    
-   try{ const completion = await openai.createChatCompletion({
-      model: "gpt-4-turbo",
-      messages: message.messages,
-    });
-    
-    let arrayOutput = chatHistory;
-    arrayOutput.push(completion.data.choices[0].message);
+    try {
+      const completion = await openai.createChatCompletion({
+        model: "o3-mini-2025-01-31",
+        messages: message.messages,
+      });
 
-    const usage = completion.data.usage;
-    const username = auth?.user;
+      let arrayOutput = chatHistory;
+      arrayOutput.push(completion.data.choices[0].message);
+
+      const usage = completion.data.usage;
+      const username = auth?.user;
       try {
-        await axios.post('/.netlify/functions/userExperience', 
-        JSON.stringify({username, usage}),
-            {
-                headers: { 'Content-Type': 'application/json' },
-                withCredentials: true
-            }
+        await axios.post(
+          "/.netlify/functions/userExperience",
+          JSON.stringify({ username, usage }),
+          {
+            headers: { "Content-Type": "application/json" },
+            withCredentials: true,
+          }
         );
-    } catch (e) {
+      } catch (e) {
         if (e.response) {
           console.log(e.response.data.message);
+        }
+      }
+      addChat(arrayOutput);
+      setLoading(false); // Stop loading
+      setError(""); // reset error message
+    } catch (e) {
+      setError(JSON.stringify(e.name + "-" + e.message));
+      addChat([]);
+      setLoading(false); // Stop loading
     }
   }
-    addChat(arrayOutput);
-    setLoading(false); // Stop loading
-    setError(""); // reset error message
-       }
-      catch (e){
-        setError(JSON.stringify(e.name+"-"+e.message))
-        addChat([])
-        setLoading(false); // Stop loading
-      } 
-    }
-    const resetButton = () => {
-      addChat([])
-      setError(""); // reset error messages
-    }
+  const resetButton = () => {
+    addChat([]);
+    setError(""); // reset error messages
+  };
   return (
     <Container className="container-gpt">
-    <div className='wrapper'>
+      <div className="wrapper">
+        <Container className="title">
+          <h3 style={{ margin: "1rem" }}>
+            o3-mini-2025-01-31 - best for coding
+          </h3>
+          <Button
+            className="reset-button"
+            variant="outline-danger"
+            onClick={() => resetButton()}
+          >
+            Reset text
+          </Button>
+        </Container>
+        <Form.Control
+          className="input"
+          as="textarea"
+          type="textarea"
+          id="content"
+          name="content"
+          onChange={handleChange}
+          value={content}
+          autoComplete="on"
+        />
+        <Button
+          style={{ marginBottom: "1rem" }}
+          variant="outline-light"
+          onClick={() => OpenAI(content)}
+        >
+          {loading ? <>Loading..</> : <>Let the magic begin</>}
+        </Button>
+        <p style={{ padding: ".1rem" }} className="error-chat">
+          {error}
+        </p>
+      </div>
 
-      <Container className='title'>
-        <h3 style={{margin: "1rem"}}>GPT-4 Turbo</h3>
-        <Button className='reset-button' variant="outline-danger" onClick={() => resetButton()}>Reset text</Button>
-      </Container>
-    <Form.Control
-    className='input'
-    as="textarea"
-    type="textarea"
-    id="content"
-    name="content"
-    onChange={handleChange}
-    value={content}
-    autoComplete="on"
-  />
-    <Button style={{marginBottom: "1rem"}} variant="outline-light" onClick={() => OpenAI(content)}>{loading ? <>Loading..</> : <>Let the magic begin</>}</Button>
-    <p style={{padding:".1rem"}} className='error-chat'>{error}</p>
-  </div>
-
-  <ChatContainer chatHistory={chatHistory}/>
-</Container>
+      <ChatContainer chatHistory={chatHistory} />
+    </Container>
   );
 }
 
